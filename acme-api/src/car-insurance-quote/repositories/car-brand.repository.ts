75baf3e90dccaf4954e86257fab04e72e91d7504
Christophe Geers <geersch@gmail.com>
2021-08-13 12:20:56 +0200

    fix: fix IDs of brand (in-memory) store

--- acme-api/src/car-insurance-quote/repositories/car-brand.repository.ts
+++ acme-api/src/car-insurance-quote/repositories/car-brand.repository.ts
@@ -8,8 +8,8 @@ export interface CarBrand {
 export class CarBrandRepository {
   private readonly brands: CarBrand[] = [
     { id: 1, name: 'Audi', minimumDriverAge: 18, yearlyPremium: 250 },
-    { id: 1, name: 'BMW', minimumDriverAge: 18, yearlyPremium: 150 },
-    { id: 1, name: 'Porsche', minimumDriverAge: 25, yearlyPremium: 500 },
+    { id: 2, name: 'BMW', minimumDriverAge: 18, yearlyPremium: 150 },
+    { id: 3, name: 'Porsche', minimumDriverAge: 25, yearlyPremium: 500 },
   ];
 
   public findById(id: number): CarBrand | undefined {
